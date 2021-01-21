2081985933b2ed7be406284d1dac9b15d4d57791
abdulhafizr <abdulhafizramadan.17@gmail.com>
2021-01-21 22:16:11 +0700

    Handle cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 require('dotenv/config');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server Running in ${PORT}`);
