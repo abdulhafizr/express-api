886816905526df7a694bb7c817b702499ca5127d
abdulhafizr <abdulhafizramadan.17@gmail.com>
2021-01-21 22:53:25 +0700

    Make auth route

--- app.js
+++ app.js
@@ -8,11 +8,17 @@ const DB_CONNECTIONS = process.env.DB_CONNECTIONS;
 
 // Handle CORS POLICY
 app.use(cors());
+// Handle request.body 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 // MongoDB connection with mongoose
 mongoose.connect(DB_CONNECTIONS, {useNewUrlParser: true, useUnifiedTopology: true});
+
+// auth Routes
+app.get('/api/user/login', (request, response) => {
+    response.send(`Login berhasil ${request.body.username}`)
+}) 
 
 // Check mongoDB connection
 let db = mongoose.connection;
