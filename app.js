545eca1a93c63dad89a7bdfe24d175235f8a5119
abdulhafizr <abdulhafizramadan.17@gmail.com>
2021-01-21 22:33:45 +0700

    Make mongodb connection with mongoose

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 require('dotenv/config');
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT;
+const DB_CONNECTIONS = process.env.DB_CONNECTIONS;
 
+// Handle CORS POLICY
 app.use(cors());
+
+// MongoDB connection with mongoose
+mongoose.connect(DB_CONNECTIONS, {useNewUrlParser: true, useUnifiedTopology: true});
+
+// Check mongoDB connection
+let db = mongoose.connection;
+// Handle mongoDB connection on error
+db.on('error', console.error.bind(console, 'Connection Database Error!'));
+// Handle mongoDB connection on success
+db.once('open', () => {
+    console.log('Connection Database Successfully')
+})
 
 app.listen(PORT, () => {
     console.log(`Server Running in ${PORT}`);
