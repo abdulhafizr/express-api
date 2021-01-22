9ed7f31c526dc135a48c7de57a721a4bc44d00cf
abdulhafizr <abdulhafizramadan.17@gmail.com>
2021-01-22 08:50:23 +0700

    Add validation to route signup

--- src/controllers/auth/controller/signup.js
+++ src/controllers/auth/controller/signup.js
@@ -1,3 +1,8 @@
+const User = require('../../../models/User');
+const { validationResult } = require('express-validator');
 module.exports = (request, response) => {
+    const errors = validationResult(request);
+
+    if(!errors.isEmpty()) return response.status(400).json({message: errors});
     response.send('Signup');
 }
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const { body } = require('express-validator');
 const app = express();
 
 const { signin, signup } = require('../controllers/auth');
 
+// Signin Route 
 app.post('/signin', signin);
-app.post('/signup', signup);
+
+// Signup Route
+app.post(
+    '/signup',
+    [
+        body('name').isLength({min: 3}),
+        body('username').isLength({min: 3}),
+        body('password').isLength({min: 6}),
+        body('email').isEmail()
+    ],
+    signup
+);
 
 module.exports = app;
