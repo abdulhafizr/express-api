e9dd96667fb1f316f356d79cbb6d1d3cb3c400f3
abdulhafizr <abdulhafizramadan.17@gmail.com>
2021-01-22 09:12:00 +0700

    Signup features V1 has finish

--- src/controllers/auth/controller/signup.js
+++ src/controllers/auth/controller/signup.js
@@ -1,8 +1,27 @@
 const User = require('../../../models/User');
+const bcrypt = require('bcryptjs');
 const { validationResult } = require('express-validator');
-module.exports = (request, response) => {
+module.exports = async (request, response) => {
     const errors = validationResult(request);
 
     if(!errors.isEmpty()) return response.status(400).json({message: errors});
-    response.send('Signup');
+
+    const { name, username, password, email } = request.body;
+
+    const emailExist = await User.findOne({email});
+
+    if(emailExist) return response.status(400).json({message: 'Email already exist!'});
+
+    const hashPassword = await bcrypt.hash(password, await bcrypt.genSalt(10));
+
+    User.create({name, username, hashPassword, email}, (error, data) => {
+
+        if(error) return response.status(500).json({message: 'Internal Server Error', error});
+
+        response.status(200).json({
+            message: 'User has created!',
+            data
+        })
+    })
+
 }
